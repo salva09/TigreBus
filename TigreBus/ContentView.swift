30f39859ffe566903b0f2cf8469c440fa1011334
salvador <salva.hg01@gmail.com>
2022-03-10 10:18:56 -0600

    Initial Commit

---
+++ TigreBus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TigreBus
+//
+//  Created by iOSLab12 on 10/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TigreBus/TigreBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TigreBusApp.swift
+//  TigreBus
+//
+//  Created by iOSLab12 on 10/03/22.
+//
+
+import SwiftUI
+
+@main
+struct TigreBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
